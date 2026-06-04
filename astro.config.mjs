fb4f49cf6b43576031978c789cf8406e64b5bee2
Hardov <1artem1nazarov1@gmail.com>
2026-06-04 13:42:26 +0300

    feat: add important things & hero/header screen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    site: "https://hardev.ru",
 
-  integrations: [mdx()]
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [mdx(), sitemap()],
 });
